93cae09781bba1ec15073bec178fd49dcb338087
marcus.vgsampaio <marcus.vgsampaio@CAS0616567W11-1.senacsp.edu.br>
2025-10-14 17:47:53 -0300

    create - DTO funcionario

---
+++ src/main/java/ZtechAplication/DTO/FuncionarioDTO.java
@@ -0,0 +1,79 @@
+package ZtechAplication.DTO;
+
+public class FuncionarioDTO {
+	
+	private Integer idFuncionario;
+	private String nomeFuncionario;
+	private String cpf;
+	
+	private String endEmail;
+	private String telefone;
+	
+	private String rua;
+	private String cep;
+	private String bairro;
+	private String cidade;
+	private int numeroCasa;
+	public Integer getIdFuncionario() {
+		return idFuncionario;
+	}
+	public void setIdFuncionario(Integer idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+	public String getNomeFuncionario() {
+		return nomeFuncionario;
+	}
+	public void setNomeFuncionario(String nomeFuncionario) {
+		this.nomeFuncionario = nomeFuncionario;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEndEmail() {
+		return endEmail;
+	}
+	public void setEndEmail(String endEmail) {
+		this.endEmail = endEmail;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public int getNumeroCasa() {
+		return numeroCasa;
+	}
+	public void setNumeroCasa(int numeroCasa) {
+		this.numeroCasa = numeroCasa;
+	}
+
+	
+}
